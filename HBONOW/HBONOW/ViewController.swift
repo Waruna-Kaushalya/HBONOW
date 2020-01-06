db9657bb32f0aedb9c42da555868b4f862dc80c8
Waruna-Kaushalya <waruna95610@gmail.com>
2020-01-07 01:58:52 +0530

    add login

--- HBONOW/HBONOW/ViewController.swift
+++ HBONOW/HBONOW/ViewController.swift
@@ -10,7 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var btn1: UIButton!
+    @IBOutlet weak var btn2: UIButton!
     override func viewDidLoad() {
+        
+        textField1.layer.cornerRadius = 9.0
+        textField1.clipsToBounds = true
+        textField1.layer.maskedCorners = [.layerMinXMinYCorner,.layerMaxXMinYCorner]
+        
+        textField2.layer.cornerRadius = 9.0
+        textField2.clipsToBounds  = true
+        textField2.layer.maskedCorners = [.layerMinXMaxYCorner,.layerMaxXMaxYCorner]
+        
+        btn1.layer.cornerRadius = 5.0
+        btn1.clipsToBounds = true
+        
+        btn2.layer.cornerRadius = 5.0
+        btn2.clipsToBounds = true
+        btn2.layer.borderWidth = 0.3
+        btn2.layer.borderColor = UIColor.white.cgColor
+//
+        
+            //[UIColor.white].CGColor;
+        
+        
+
+        
+        
+        
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
