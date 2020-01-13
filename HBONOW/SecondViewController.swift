d3ecbd822260ab3e4047c1d27a999a246ae7deaf
Waruna-Kaushalya <waruna95610@gmail.com>
2020-01-13 13:56:27 +0530

    add v2

---
+++ HBONOW/SecondViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SecondViewController.swift
+//  HBONOW
+//
+//  Created by Waruna Kaushalya on 1/13/20.
+//  Copyright © 2020 Waruna Kaushalya. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+  
+}
